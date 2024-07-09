ea49eadb5f2503d8ce135a24a8521cbb8681686d
obervinov <obervinov@pm.me>
2024-07-09 13:27:54 +0400

    fixed constants

--- users/__init__.py
+++ users/__init__.py
@@ -7,15 +7,14 @@ which provides functionality for managing users and rate limiting.
 from .ratelimits import RateLimiter
 from .users import Users
 from .storage import Storage
-from .constants import VAULT_CONFIG_PATH, VAULT_DATA_PATH, USER_STATUS_ALLOW, USER_STATUS_DENY
+from .constants import USERS_VAULT_CONFIG_PATH, USER_STATUS_ALLOW, USER_STATUS_DENY
 from .exceptions import WrongUserConfiguration, VaultInstanceNotSet, FailedDeterminateRateLimit, FailedStorageConnection, StorageInstanceNotSet
 
 __all__ = [
     'RateLimiter',
     'Users',
     'Storage',
-    'VAULT_CONFIG_PATH',
-    'VAULT_DATA_PATH',
+    'USERS_VAULT_CONFIG_PATH',
     'USER_STATUS_ALLOW',
     'USER_STATUS_DENY',
     'WrongUserConfiguration',
--- users/constants.py
+++ users/constants.py
@@ -3,7 +3,7 @@ This module contains constant values used in the application.
 """
 
 # Constants for Vault paths
-USERS_VAULT_CONFIGURATION_PATH = "configuration/users"
+USERS_VAULT_CONFIG_PATH = "configuration/users"
 
 # User statuses
 USER_STATUS_ALLOW = "allowed"
