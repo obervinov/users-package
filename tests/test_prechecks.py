f3449835000bb80cc2b806a95c44834046d0f7bb
obervinov <obervinov@pm.me>
2024-07-09 21:30:58 +0400

    modified:   tests/test_prechecks.py

--- tests/test_prechecks.py
+++ tests/test_prechecks.py
@@ -5,19 +5,19 @@ import pytest
 
 
 @pytest.mark.order(19)
-def test_check_init_vault_conf(users):
+def test_check_init_vault_conf(users_instance):
     """
     Checking the functionality of the init method of the class when the Vault configuration is passed
     """
-    assert isinstance(users.vault, object)
-    print(users.vault)
-    assert users.vault.read_secret(path='data/users/testUser1') is not None
+    assert isinstance(users_instance.vault, object)
+    print(users_instance.vault)
+    assert users_instance.vault.read_secret(path='data/users/testUser1') is not None
 
 
 @pytest.mark.order(20)
-def test_check_init_postgres_data(users):
+def test_check_init_postgres_data(users_instance):
     """
     Checking the functionality of the init method of the class when the Postgres test data is passed
     """
-    assert isinstance(users.vault, object)
-    assert users.storage.get_user_requests(user_id='testUser1') is not None
+    assert isinstance(users_instance.vault, object)
+    assert users_instance.storage.get_user_requests(user_id='testUser1') is not None
