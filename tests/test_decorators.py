4cfc7f03f2961f59c60ef3513a7bda7d03421f7f
obervinov <bervinov.ob@gmail.com>
2024-12-19 18:29:15 +0400

    added mote tests for decorator

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -47,44 +47,75 @@ def test_access_control_decorator(users_instance):
     """
     Verify the access control decorator for the user.
     """
+
+    # the user is allowed access to the bot
     message = MockMessage(user_id='testUser24', username='TestUser24', chat_id='testChat24', message_id=random.randint(1, 9999))
 
     @users_instance.access_control(flow='auth')
-    def allowed_function(message: object, access_result: dict = None):
+    def allowed_exist_user_function(message: object, access_result: dict = None):
         print(message)
         return access_result
-    assert allowed_function(message) == {'access': users_instance.user_status_allow}
+    assert allowed_exist_user_function(message) == {'access': users_instance.user_status_allow}
 
+    # the user is explicitly blocked from accessing the bot
     message = MockMessage(user_id='testUser25', username='TestUser25', chat_id='testChat25', message_id=random.randint(1, 9999))
 
     @users_instance.access_control(flow='auth')
-    def denied_function(message: object, access_result: dict = None):
+    def blocked_exist_user_function(message: object, access_result: dict = None):
         print(message)
-        return access_result
-    assert denied_function(message) is None
+        print(access_result)
+        return 'unauthorized access'
+    assert blocked_exist_user_function(message) is None
 
+    # user has a specified access role
     message = MockMessage(user_id='testUser24', username='TestUser24', chat_id='testChat24', message_id=random.randint(1, 9999))
 
     @users_instance.access_control(role_id='admin_role', flow='authz')
-    def allowed_role_function(message: object, access_result: dict = None):
+    def allowed_exist_user_with_role_function(message: object, access_result: dict = None):
         print(message)
         return access_result
-    assert allowed_role_function(message) == {
+    assert allowed_exist_user_with_role_function(message) == {
         'access': users_instance.user_status_allow, 'permissions': users_instance.user_status_allow, 'rate_limits': None
     }
 
-    message = MockMessage(user_id='testUser125', username='TestUser125', chat_id='testChat125', message_id=random.randint(1, 9999))
+    # the user has a role but not the one requested
+    message = MockMessage(user_id='testUser24', username='TestUser24', chat_id='testChat24', message_id=random.randint(1, 9999))
+
+    @users_instance.access_control(role_id='other_role', flow='authz')
+    def allowed_exist_user_without_role_function(message: object, access_result: dict = None):
+        print(message)
+        print(access_result)
+        return 'unauthorized access'
+    assert allowed_exist_user_without_role_function(message) is None
+
+    # the user is explicitly blocked from access and the user has no roles
+    message = MockMessage(user_id='testUser25', username='TestUser25', chat_id='testChat25', message_id=random.randint(1, 9999))
 
     @users_instance.access_control(role_id='admin_role', flow='authz')
-    def denied_role_function(message: object, access_result: dict = None):
+    def blocked_exist_user_without_role_function(message: object, access_result: dict = None):
         print(message)
-        return access_result
-    assert denied_role_function(message) is None
+        print(access_result)
+        return 'unauthorized access'
+    assert blocked_exist_user_without_role_function(message) is None
 
-    message = MockMessage(user_id='testUser125', username='TestUser125', chat_id='testChat125', message_id=random.randint(1, 9999))
+    # non-existent random user configuration
+    random_user_id = f"testUser{random.randint(100, 10000)}"
+    message = MockMessage(user_id=random_user_id, username=random_user_id, chat_id=random_user_id, message_id=random.randint(1, 9999))
 
     @users_instance.access_control(flow='auth')
-    def denied_unknown_user_function(message: object, access_result: dict = None):
+    def does_not_exist_user_function(message: object, access_result: dict = None):
         print(message)
-        return access_result
-    assert denied_unknown_user_function(message) is None
+        print(access_result)
+        return 'unauthorized access'
+    assert does_not_exist_user_function(message) is None
+
+    # non-existent random user configuration without role
+    random_user_id = f"testUser{random.randint(100, 10000)}"
+    message = MockMessage(user_id=random_user_id, username=random_user_id, chat_id=random_user_id, message_id=random.randint(1, 9999))
+
+    @users_instance.access_control(role_id='admin_role', flow='authz')
+    def does_not_exist_user_without_role_function(message: object, access_result: dict = None):
+        print(message)
+        print(access_result)
+        return 'unauthorized access'
+    assert does_not_exist_user_without_role_function(message) is None
