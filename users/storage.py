8932d8b39c7640bbfee51847d8d2d033b1bbcb70
obervinov <bervinov.ob@gmail.com>
2025-02-18 17:27:45 +0400

    fixed error in db configuration validation

--- users/storage.py
+++ users/storage.py
@@ -91,14 +91,15 @@ class Storage:
             psycopg2.connect: The database connection object.
         """
         if self.vault and not self.db_connection:
-            required_keys = {"configuration": ["host", "port", "dbname"], "credentials": ["username", "password"]}
+            required_keys_configuration = {"host", "port", "dbname"}
+            required_keys_credentials = {"username", "password"}
             db_configuration = self.vault['instance'].kv2engine.read_secret(path='configuration/database')
             db_credentials = self.vault['instance'].dbengine.generate_credentials(role=self.vault.get('role', 'undefined'))
 
             if not db_configuration or not db_credentials:
                 raise ValueError('vault: database configuration or credentials are missing')
 
-            missing_keys = (required_keys['configuration'] - set(db_configuration.keys())) | (required_keys['credentials'] - set(db_credentials.keys()))
+            missing_keys = (required_keys_configuration - set(db_configuration.keys())) | (required_keys_credentials - set(db_credentials.keys()))
             if missing_keys:
                 raise KeyError("vault: missing keys in the database configuration or credentials: {missing_keys}")
 
