40f669b08c4471ca411836742e4d1a24d913dd84
obervinov <bervinov.ob@gmail.com>
2025-02-21 13:10:18 +0400

    modified:   tests/test_dbengine.py

--- tests/test_dbengine.py
+++ tests/test_dbengine.py
@@ -1,7 +1,5 @@
 """This file contains tests for check connection to database with Vault Database Engine"""
 import pytest
-from psycopg2 import connect
-
 
 @pytest.mark.order(17)
 def test_check_dbengine_connection(users_instance_dbengine):
@@ -15,7 +13,7 @@ def test_check_dbengine_connection(users_instance_dbengine):
     assert isinstance(users_instance_dbengine.storage.vault.role, str)
     assert users_instance_dbengine.storage.vault.role == 'test-role'
     # Check the connection to the database engine
-    assert isinstance(users_instance_dbengine.storage.connection, connect)
+    assert users_instance_dbengine.storage.connection is not None
     assert users_instance_dbengine.storage.cursor is not None
     # Check query execution
     users_instance_dbengine.storage.cursor.execute("SELECT id FROM users LIMIT 1")
